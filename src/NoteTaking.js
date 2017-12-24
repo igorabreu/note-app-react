e03ff399d55df2f0023cca2738d2187221c1d973
Igor Abreu <igor.abreu.reis@gmail.com>
2017-12-24 07:58:10 -0200

    add 'remove notes'

--- src/NoteTaking.js
+++ src/NoteTaking.js
@@ -15,14 +15,20 @@ class NoteTaking extends Component {
       },
     ],
     noteTitle: '',
-    selectedNotes: [ 0 ],
+    selectedNotes: [],
   }
+
+/*  componentDidMount() {
+    var self = this;
+    setInterval(function(){ console.log(self.state.selectedNotes); }, 1000);
+  }*/
 
   render() {
     return (
       <div className="App">
         <input type='text' onChange={ this.onTitle } value={ this.state.noteTitle }/>
         <button onClick={ this.addNote }>add</button>
+        <button onClick={ this.removeSelected }>remove</button>
         { this.renderNotes() }
       </div>
     );
@@ -30,6 +36,10 @@ class NoteTaking extends Component {
 
   onTitle = (e) => {
     this.setState({ noteTitle: e.target.value });
+  }
+
+  onText = (e) => {
+
   }
 
   addNote = () => {
@@ -47,15 +57,46 @@ class NoteTaking extends Component {
     return this.state.noteList.map((note, index) => {
       return(
         <li key={index}>
-          <input type='checkbox' defaultChecked={ this.isSelected(index) } />
+          <input type='checkbox' defaultChecked={ this.isSelected(index) } onClick={ () => this.selectNote(index) } />
           <h3>{ note.title }</h3>
         </li>
       )
     })
   }
 
+  selectNote = (index) => {
+    let newSelection;
+    if (this.isSelected(index)){
+      this.setState({ selectedNotes: this.state.selectedNotes.filter(item => item !== index) });
+      return;
+    }
+    newSelection = this.state.selectedNotes.concat(index);
+    this.sortArray(newSelection);
+    this.setState({ selectedNotes: newSelection });
+  }
+
   isSelected = (index) => {
-    return this.state.selectedNotes.includes(index);
+    return this.state.selectedNotes.indexOf(index) > -1;
+  }
+
+  removeSelected = () => {
+    let newList = this.state.noteList;
+    for (var i = this.state.selectedNotes.length - 1; i >= 0; i--) {
+      newList.splice(this.state.selectedNotes[i], 1);
+    }
+    this.setState({ noteList: newList, selectedNotes: [] });
+  }
+
+  sortArray = (array) => {
+    return array.sort((a, b) => {
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+    });
   }
 }
 
